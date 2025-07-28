ef114c97c5d3fc132a5687bf8986ae9a01bfa80e
Stefan Horvath <tejohth@gmail.com>
2025-07-28 18:04:04 +0200

    CHG: remove redundant initial code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: '01_auth_app',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
